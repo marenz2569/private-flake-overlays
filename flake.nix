316f9c9f3d79ccc52f8148b616789c38348cb58e
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2023-11-27 13:32:52 +0100

    initial nixosConfiguration overlay

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Add confidential or private configurations and packages to your public flake.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+  outputs = { self, nixpkgs }: {
+    lib.overlayFlake = url: other: let
+      flake = if builtins ? currentSystem then builtins.getFlake url else {};
+        # inject nixosconfigurations if they don't already exist
+        combinedNixosConfigurations = nixpkgs.lib.zipAttrs [ (other.nixosConfigurations or {}) (flake.nixosConfigurations or {}) ];
+        findDuplicate = n: v: builtins.length v > 1 && abort "nixosConfiguration ${n} cannot be duplicate in both public and private flake";
+
+        filteredNixosConfigurations = nixpkgs.lib.filterAttrs (k: v: k == "nixosConfigurations") flake;
+      in other // filteredNixosConfigurations;
+  };
+}
